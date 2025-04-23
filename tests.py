da7feb6e99db22bbc48947904f2f806a8493819c
Sivabalan <sivansbhfe@gmail.com>
2025-04-23 18:30:42 +0530

    first push

---
+++ tests.py
@@ -0,0 +1,15 @@
+import unittest
+
+from pyasn1_modules.rfc2985 import contentType
+
+import app
+
+class BasicTestCase(unittest.TestCase):
+    def test_home(self):
+        tester=app.app.test_client(self)
+        response = tester.get('/',content_type='html/text')
+        self.assertEqual(response.status_code,200)
+        self.assertIn(b'Hello',response.data)
+
+if __name__ == "__main__":
+    unittest.main()
